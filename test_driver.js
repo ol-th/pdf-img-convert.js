2559b416746a80a4665883ccefd30d9322c7be95
Oliver Thwaites <oliver.thwaites@student.manchester.ac.uk>
2020-06-30 16:23:42 +0100

    rename

--- test_driver.js
+++ test_driver.js
@@ -1,11 +1,24 @@
-var pdf2img = require("./pdf2img.js");
+var pdf2img = require("./pdf-to-image.js");
 var fs = require("fs");
 
-pdf2img.convert('https://gahp.net/wp-content/uploads/2017/09/sample.pdf').then(function(pdfArray) {
-  console.log("saving")
+// var output = pdf2img.convert('https://gahp.net/wp-content/uploads/2017/09/sample.pdf')
+//
+//
+// output.then(function(pdfArray) {
+//   console.log("saving")
+//   for (i = 0; i < pdfArray.length; i++){
+//     fs.writeFile("output"+i+".png", pdfArray[i], function (error) {
+//       if (error) { console.error("Error: " + error); }
+//     }); //writeFile
+//   } // for
+// });
+
+(async function () {
+  pdfArray = await pdf2img.convert('https://gahp.net/wp-content/uploads/2017/09/sample.pdf', {width: 1080, page_numbers: [1]});
+  console.log("saving");
   for (i = 0; i < pdfArray.length; i++){
     fs.writeFile("output"+i+".png", pdfArray[i], function (error) {
       if (error) { console.error("Error: " + error); }
     }); //writeFile
   } // for
-});
+})();
