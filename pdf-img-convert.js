6d0dbfe930e963c65d754500e75260d66aaa81c8
Oliver Thwaites <oliver.thwaites@student.manchester.ac.uk>
2022-05-04 17:19:14 +0100

    Update main package

--- pdf-img-convert.js
+++ pdf-img-convert.js
@@ -22,9 +22,9 @@ SOFTWARE.
 
 */
 
-const fetch = require('node-fetch');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const isURL = require('is-url');
-const pdfjs = require('pdfjs-dist/es5/build/pdf.js');
+const pdfjs = require('pdfjs-dist/legacy/build/pdf.js');
 const Canvas = require("canvas");
 const assert = require("assert").strict;
 const fs = require("fs");
