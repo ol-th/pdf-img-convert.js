f7b7a15b3d5008b8026b3cf97846d639d29ed254
${Mr.DJA} <42304709+iMrDJAi@users.noreply.github.com>
2022-06-06 15:26:17 +0100

    🏷️ Added type definition

---
+++ pdf-img-convert.d.ts
@@ -0,0 +1,13 @@
+module.exports.convert = async function (
+  pdf: string|Uint8Array|Buffer,
+  conversion_config?: {
+    /** Number in px */
+    width: number
+    /** Number in px */
+    height: number
+    /** A list of pages to render instead of all of them */
+    page_numbers: number[]
+    /** Output as base64 */
+    base64: boolean
+  }
+): (string|Uint8Array)[] {}
